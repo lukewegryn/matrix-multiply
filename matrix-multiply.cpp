30c1ecc51b02d45f76f5c4b79a62171b6eab7592
Luke <lwegryn@vt.edu>
2014-10-30 14:00:37 -0400

    Update

--- matrix-multiply.cpp
+++ matrix-multiply.cpp
@@ -3,34 +3,117 @@
 #include <QTextStream>
 #include "QStream.h"
 #include <QList>
+#include <QVarLengthArray>
+#include <QPair>
+#include <pthread.h>
 
-bool argumentParse(int argc, char *argv[], QTextStream &matrix1, QTextStream &matrix2)
+struct matrix {
+    QTextStream matrixStream;
+    QList<QList<double> > matrix;
+
+
+};
+
+QStringList argumentParse(int argc, char *argv[])
 {
-	bool isSuccess = true;
 	ArgumentList al(argc, argv);
 	al.takeFirst();
+	QStringList argumentList;
 	int i = 0;
-	while(!al.isEmpty())
+	if(al.isEmpty())
 	{
-		QString fileName = al.takeFirst();
-		QFile data(fileName);
-		if(!data.open(QFile::ReadOnly | QIODevice::Text))
-		{
-			isSuccess = false;
-			break;
-		}
-		else
-		{
-			matrix[i] = QTextStream(&data);
-		}
-		i++;
+		qout << "You need to supply two valid files." << endl;
+		exit(0);
 	}
-
-	return isSuccess;
+	else
+	{
+		while(!al.isEmpty())
+		{
+			argumentList.append(al.takeFirst());
+		}	
+	}
+	return argumentList;
 }
+
+void *readData(void *List)
+{
+	while(!matrixStream1.atEnd()){
+		QString line = matrixStream1.readLine();
+		QStringList tempList  = line.split(rx);
+		QList<double> column;
+		for(int i = 0; i < tempList.size(); i++)
+		{
+			column.append(tempList[i].toDouble());
+		}
+		matrix1.append(column);
+		mDim_1.second = tempList.size();
+		mDim_1.first++;
+	}
+}
+
+
 int main(int argc, char* argv[])
 {
-	QList<QTextStream*> matrix; //a list of the two matrices
-	qout << argumentParse(argc, argv, matrix) << endl;
+	QStringList fileList; //a list of the two matrices
+	fileList = argumentParse(argc, argv);
+
+	QFile file1(fileList[0]);
+	QFile file2(fileList[1]);
+
+	if(!file1.open(QIODevice::ReadOnly | QIODevice::Text))
+	{
+		qout << "You did not enter a valid file." << endl;
+		exit(0);
+	}
+	if(!file2.open(QIODevice::ReadOnly | QIODevice::Text))
+	{
+		qout << "You did not enter a valid file." << endl;
+		exit(0);
+	}
+
+
+	QList<QList<double> > matrix1;
+	QList<QList<double> > matrix2;
+	QPair<int, int> mDim_1; //m rows, n columns
+	QPair<int, int> mDim_2; //m rows, n columns
+	mDim_1.first = 0;
+	mDim_1.second = 0;
+	mDim_2.first = 0;
+	mDim_2.second = 0;
+
+	QTextStream matrixStream1(&file1);
+	QTextStream matrixStream2(&file2);
+
+	QRegExp rx("(\\s|\\t)");
+
+	
+
+	for(int i = 0; i < mDim_1.first; i++)
+	{
+		for(int j = 0; j < mDim_1.second; j++)
+		{
+			qout << matrix1[i][j]<< "\t";
+		}
+		qout << endl;
+	}
+
+	qout << mDim_1.first << "x" << mDim_1.second << endl;
+
+	//QVarLengthArray<
+
+
 
 }
+
+/*while(!matrixStream1.atEnd()){
+		QString line = matrixStream1.readLine();
+		QStringList tempList  = line.split(rx);
+		QList<double> column;
+		for(int i = 0; i < tempList.size(); i++)
+		{
+			column.append(tempList[i].toDouble());
+		}
+		matrix1.append(column);
+		mDim_1.second = tempList.size();
+		mDim_1.first++;
+	}*/
